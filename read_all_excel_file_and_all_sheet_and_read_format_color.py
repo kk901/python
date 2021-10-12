5c9c802b017770ae7848d1c41ce0d4392cec31e0
kk901 <k.chris.0605@gmail.com>
2021-10-12 15:50:58 +0900

    Update and rename read_all_excel_file_and_all_sheet.py to read_all_excel_file_and_all_sheet_and_read_format_color.py

--- read_all_excel_file_and_all_sheet_and_read_format_color.py
+++ read_all_excel_file_and_all_sheet_and_read_format_color.py
@@ -23,6 +23,19 @@ for n in tqdm(file):
         target_sheet = k
         input_sheet_df = input_book.parse(target_sheet,
                                           skiprows = skiprows)
+        wb = openpyxl.load_workbook(input_path)
+        sheet = wb[input_sheet_name]
+        max_raw = len(input_sheet_df)
+        mylist = []
+        for i in range(skiprows+2,max_raw+skiprows+1):
+            cell = sheet.cell(row=i, column=4)
+            bgcolor = cell.fill.bgColor.value
+            # 空白の書式の色の場合True、それ以外はFalse
+            if bgcolor=='00000000':
+                mylist.append(True)
+            else:
+                mylist.append(False)
+        tmp_df['format_no_color'] =   mylist
         input_sheet_df.rename(columns={'Unnamed: 1': 'xxx1', 
                            'Unnamed: 2': 'xxx2',
                            'Unnamed: 4': 'xxx3'
