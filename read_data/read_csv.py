82bf2160af064e38d92253b6b317cf22beefece4
kk901 <k.chris.0605@gmail.com>
2021-12-24 10:29:02 +0900

    Update read_csv.py

--- read_data/read_csv.py
+++ read_data/read_csv.py
@@ -1,6 +1,10 @@
-def read_csv(input_path)
+def read_csv_time(input_path):
     start = time.time()
     df = pd.read_csv(input_path , encoding='utf_8_sig')
     elapsed_time = time.time() - start
+    print(input_paht)
     print ("elapsed_time:{0}".format(elapsed_time) + "[sec]")
     return df
+
+input_path = 'input/input.csv'
+input_df = read_csv_time(input_path)
